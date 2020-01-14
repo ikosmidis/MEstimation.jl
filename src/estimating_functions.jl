b3bbbf31a6e84b74057768f5f2aa229a765eece8
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2020-01-14 09:47:02 +0000

    Moved fit functions in fit.jl

--- src/estimating_functions.jl
+++ src/estimating_functions.jl
@@ -49,53 +49,6 @@ function get_estimating_function(data::Any,
     end
 end
 
-"""   
-    fit(template::estimating_function_template, data::Any, theta::Vector; estimation_method::String = "M", br_method::String = "implicit_trace", nlsolve_arguments...)
-
-Fit an [`estimating_function_template`](@ref) on `data` using M-estimation ([keyword argument](https://docs.julialang.org/en/v1/manual/functions/#Keyword-Arguments-1) `estimation_method = "M"`; default) or RBM-estimation (reduced-bias M estimation; [keyword argument](https://docs.julialang.org/en/v1/manual/functions/#Keyword-Arguments-1) `estimation_method = "RBM"`). Bias reduction is either through the solution of the empirically adjusted estimating functions in Kosmidis & Lunardon (2020) ([keyword argument](https://docs.julialang.org/en/v1/manual/functions/#Keyword-Arguments-1) `br_method = "implicit_trace"`; default) or by subtracting an estimate of the bias from the M-estimates ([keyword argument](https://docs.julialang.org/en/v1/manual/functions/#Keyword-Arguments-1) `br_method = "explicit_trace"`). The bias-reducing adjustments and the bias estimate are constructed internally using automatic differentiation (using the [ForwardDiff](https://github.com/JuliaDiff/ForwardDiff.jl) package).
-
-The solution of the estimating equations or the adjusted estimating equations is done using the [**NLsolve**](https://github.com/JuliaNLSolvers/NLsolve.jl) package. Arguments can be passed directly to `NLsolve.nlsolve` through [keyword arguments](https://docs.julialang.org/en/v1/manual/functions/#Keyword-Arguments-1). See the [NLsolve README](https://github.com/JuliaNLSolvers/NLsolve.jl) for more information on available options.
-"""
-function fit(template::estimating_function_template,
-             data::Any,
-             theta::Vector;
-             estimation_method::String = "M",
-             br_method::String = "implicit_trace",
-             nlsolve_arguments...)
-    if (estimation_method == "M")
-        br = false
-    elseif (estimation_method == "RBM")
-        if (br_method == "implicit_trace")
-            br = true
-        elseif (br_method == "explicit_trace")
-            br = false
-        else
-            error(br_method, " is not a recognized bias-reduction method")
-        end
-    else
-        error(estimation_method, " is not a recognized estimation method")
-    end
-    ## down the line when det is implemented we need to be passing the
-    ## bias reduction method to estimating_function
-    ef = get_estimating_function(data, template, br)   
-    out = nlsolve(ef, theta; nlsolve_arguments...)
-    if (estimation_method == "M")
-        theta = out.zero
-    elseif (estimation_method == "RBM") 
-        if (br_method == "implicit_trace") 
-            theta = out.zero
-        elseif (br_method == "explicit_trace")
-            quants = ef_quantities(out.zero, data, template, true)
-            adjustment = quants[1]
-            jmat_inv = quants[2]
-            theta = out.zero + jmat_inv * adjustment
-            ## Reset br
-            br = true
-        end
-    end
-    GEEBRA_results(out, theta, data, template, br, false, br_method)
-end
-
 function ef_quantities(theta::Vector,
                        data::Any,
                        template::estimating_function_template,
